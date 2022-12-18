2c0e4ca1e2050a6794da8704a2e6d230ab0a90b1
VictorMFL <vm859222@gmail.com>
2022-12-18 19:29:02 -0300

    menu mobile quase pronto

---
+++ index.js
@@ -0,0 +1,8 @@
+let menuLista = document.querySelector('div.menu')
+let menu = document.querySelector('img#menu-hamburguer')
+
+
+function clicou() {
+    menuLista.style.display = 'block'
+    menu.style.display = 'none'
+}
