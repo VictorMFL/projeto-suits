95a74416038455824a356c339bcc371da28e7d81
VictorMFL <vm859222@gmail.com>
2022-12-19 11:12:14 -0300

    Menu mobile feito

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let menuLista = document.querySelector('div.menu')
 let menu = document.querySelector('img#menu-hamburguer')
+let iconeFechar = document.querySelector('img#fechar-menu')
 
 
-function clicou() {
+function abrirMenu() {
     menuLista.style.display = 'block'
     menu.style.display = 'none'
+    iconeFechar.style.display = 'block'
+}
+
+function fecharMenu() {
+    menuLista.style.display = 'none'
+    menu.style.display = 'block'
 }
